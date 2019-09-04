1c396e6ee9e4bdfff6ab9b8f4c90130041d3ce65
galit <galit1321@gmail.com>
2019-09-04 14:58:38 +0300

    work with mongo and cors solved 2 commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Data = require("./data");
-
+const cors = require('cors');
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
@@ -17,53 +16,40 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
+app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 router.get("/", (req, res) => {
   res.json({ message: "HELLOW WORLDUUHHHH" });
 });
 
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
 
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-//router.post("/putData", (req, res) => {
+const DataSet=[];
+router.post('/putData', (req, res) => {
   let data = new Data();
-  console.log("hello");
-  const  id=22;
-  const message  = "22";
-  if ((!id && id !== 0) || !message) {
-    console.log("fail");
+  console.log("dsd");
+  const { id, name,password } = req.body;
+  console.log( id, name,password );
+  if ((!id && id !== 0) || !name|| !password) {
     return res.json({
       success: false,
-      error: "INVALID INPUTS"
+      error: 'INVALID INPUTS',
     });
   }
-  data.message = message;
+  data.name = name;
+  data.password=password;
   data.id = id;
   data.save(err => {
     if (err) console.log("fail");  
     return console.log( "success true" );
   });
 
+
+});
 
 app.use("/api", router);
 
