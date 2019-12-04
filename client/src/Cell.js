281f05ef23722c186baa9be17391191fbf649df8
galit <galit1321@gmail.com>
2019-12-04 14:33:41 +0200

    commit table

--- client/src/Cell.js
+++ client/src/Cell.js
@@ -4,9 +4,15 @@ import "./css/Cell.css";
 
 class Cell extends Component{
     render(){
+        const data=this.props.parm;
+        console.log(data);
+        if (data.full){
+            return (<div className={data.kind}>{data.id}
+                </div>);
+        }
         return (
-           <div className="cell">{this.props.num}
-           </div>
+            <div className="emptyCell">
+            </div>       
         );
     }
 }
--- client/src/Game.js
+++ client/src/Game.js
@@ -5,18 +5,57 @@ import Cell from "./Cell"
 class Game extends Component{
     constructor(props){
         super(props);
+        let data=[];
         this.state={
             score:0,
             name:props.name,
-            data:[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+            data: data,
         }
     }
 
+    addElem(){
+        let rr1,rc1;
+        do {
+            rr1=Math.floor(Math.random() * 4); 
+            rc1=Math.floor(Math.random() * 4);
+        }
+        while (this.state.data[rr1][rc1].full);
+        this.setState(prevState => ({
+            data: {
+                ...prevState.data,
+                [prevState.data[rr1][rc1].full]: e.target.value,
+            },
+        })); 
+    }
+
+    changeCell =()=>{
+        this.data[0][0].id=3;
+
+    
+    }
+    createData() {
+        let res=[];
+        let id=1;
+        for (let i=0;i<4;i++){
+            for (let j=0;j<4;j++){
+            res.push({id:id , kind: "cell" ,full: false,});
+            id++;
+        }
+        this.state.data.push(res);
+        res=[];
+        }
+        this.addElem();
+        this.addElem();
+    }
+
     componentDidMount() {
+        this.createData();
         fetch('http://localhost:3001/api/getScore')
             .then(result => result.json())
             .then(res => this.setState({score:res.score}))
     }
+
+
     render(){
         const score=this.state.score;
         const data= this.state.data;
@@ -25,7 +64,6 @@ class Game extends Component{
         <h1> {score}</h1>
         <p1> Welcome to the game </p1>   
         </div>
-        
         <table>
         <tbody>
           {data.length <= 0
@@ -33,15 +71,15 @@ class Game extends Component{
             : data.map(dat => (
                 <tr  key={dat}  >
                     {dat.map(elem => (
-                        <th > <Cell  kind="cell" num={elem} /> </th>
+                        <th> 
+                        <Cell parm={elem} /> 
+                        </th>
                     ))}
                 </tr>
               ))}
               </tbody>
                 </table>
-            
-
-        
+                    <button onClick={this.changeCell} > ClickMe</button>
     
         </div>);
 
